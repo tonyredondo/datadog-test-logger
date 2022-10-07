701fcc5d18fd2a0742e8a016e1573a079185a0c8
Tony Redondo <tony.redondo@datadoghq.com>
2022-10-08 01:16:18 +0200

    changes

--- src/DatadogTestLogger/DatadogTestResultSerializer.cs
+++ src/DatadogTestLogger/DatadogTestResultSerializer.cs
@@ -4,7 +4,9 @@
 
 namespace DatadogTestLogger;
 
+using System.Reflection;
 using System.Text.RegularExpressions;
+using Datadog.Trace.Ci;
 using Spekt.TestLogger.Core;
 
 internal class DatadogTestResultSerializer : ITestResultSerializer
@@ -58,6 +60,91 @@ internal class DatadogTestResultSerializer : ITestResultSerializer
                 "NETFramework461" => "4.6.1",
                 _ => runConfiguration.TargetFramework
             };
+        }
+
+        var groupedResults = results
+            .OrderBy(i => i.StartTime)
+            .ThenBy(i => i.EndTime)
+            .GroupBy(g => g.AssemblyPath).ToArray();
+
+        foreach (var resultByAssembly in groupedResults)
+        {
+            string testModule = AssemblyName.GetAssemblyName(resultByAssembly.Key).Name!;
+            TestModule? module = null;
+
+            var resultBySuiteGroup = resultByAssembly.GroupBy(g => g.FullTypeName).ToArray();
+            foreach (var resultBySuite in resultBySuiteGroup)
+            {
+                string testSuite = resultBySuite.Key;
+                TestSuite? suite = null;
+                foreach (var result in resultBySuite)
+                {
+                    if (module is null)
+                    {
+                        var testFramework = string.Empty;
+                        if (result.TestCase.ExecutorUri.Host.IndexOf("xunit", StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            testFramework = "xUnit";
+                        }
+                        else if (result.TestCase.ExecutorUri.Host.IndexOf("nunit", StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            testFramework = "NUnit";
+                        }
+                        else if (result.TestCase.ExecutorUri.Host.IndexOf("mstest", StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            testFramework = "MSTestV2";
+                        }
+
+                        module = TestModule.Create(testModule, testFramework, "N/A", runConfiguration.StartTime);
+                        module.SetTag("runtime.name", runtimeName);
+                        module.SetTag("runtime.version", runtimeVersion);
+                    }
+
+                    if (suite is null)
+                    {
+                        suite = module.GetOrCreateSuite(testSuite, result.StartTime);
+                    }
+
+                    string testName = result.Method;
+
+                    var test = suite.CreateTest(testName, result.StartTime);
+
+                    // Traits
+                    if (result.Traits.Any())
+                    {
+                        var traits = result.Traits.GroupBy(k => k.Name).ToDictionary(k => k.Key, v => v.Select(i => i.Value).ToList());
+                        test.SetTraits(traits);
+                    }
+
+                    // Set status
+                    if (result.Outcome == Microsoft.VisualStudio.TestPlatform.ObjectModel.TestOutcome.Passed)
+                    {
+                        test.Close(TestStatus.Pass, result.Duration);
+                    }
+                    else if (result.Outcome == Microsoft.VisualStudio.TestPlatform.ObjectModel.TestOutcome.Skipped)
+                    {
+                        // xUnit skipped message is stored here
+                        if (result.Messages.FirstOrDefault(m => m.Category == "StdOutMsgs") is { } xUnitSkipMessage)
+                        {
+                            test.Close(TestStatus.Skip, result.Duration, xUnitSkipMessage.Text);
+                        }
+                        else
+                        {
+                            test.Close(TestStatus.Skip, result.Duration, result.ErrorMessage);
+                        }
+                    }
+                    else if (result.Outcome == Microsoft.VisualStudio.TestPlatform.ObjectModel.TestOutcome.Failed ||
+                             result.Outcome == Microsoft.VisualStudio.TestPlatform.ObjectModel.TestOutcome.NotFound)
+                    {
+                        test.SetErrorInfo("Exception", result.ErrorMessage, result.ErrorStackTrace);
+                        test.Close(TestStatus.Fail, result.Duration);
+                    }
+                }
+
+                suite?.Close(resultBySuite.Last().EndTime.Subtract(suite.StartTime.DateTime));
+            }
+
+            module?.Close(runConfiguration.EndTime.Subtract(runConfiguration.StartTime));
         }
 
         return string.Empty;
