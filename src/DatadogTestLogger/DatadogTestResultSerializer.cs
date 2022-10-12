b719084bcce2915417ae8b5dd0021f9b2af0ce97
Tony Redondo <tony.redondo@datadoghq.com>
2022-10-12 23:04:52 +0200

    Changes

--- src/DatadogTestLogger/DatadogTestResultSerializer.cs
+++ src/DatadogTestLogger/DatadogTestResultSerializer.cs
@@ -2,6 +2,9 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+using System.Collections;
+using System.Text;
+
 namespace DatadogTestLogger;
 
 using System.Reflection;
@@ -11,17 +14,15 @@ using Spekt.TestLogger.Core;
 
 internal class DatadogTestResultSerializer : ITestResultSerializer
 {
-    public DatadogTestResultSerializer()
+    public string Serialize(LoggerConfiguration loggerConfiguration, TestRunConfiguration runConfiguration, List<TestResultInfo> results, List<TestMessageInfo> messages)
     {
+        var output = new StringBuilder();
+
         Environment.SetEnvironmentVariable("DD_CIVISIBILITY_ENABLED", "true");
         Environment.SetEnvironmentVariable("DD_CIVISIBILITY_AGENTLESS_ENABLED", "true");
         Environment.SetEnvironmentVariable("DD_API_KEY", this.GetLoggerApiKey());
         Environment.SetEnvironmentVariable("DD_CIVISIBILITY_LOGS_ENABLED", "true");
-    }
 
-    public string Serialize(LoggerConfiguration loggerConfiguration, TestRunConfiguration runConfiguration, List<TestResultInfo> results, List<TestMessageInfo> messages)
-    {
-        var response = string.Empty;
         var runtimeName = string.Empty;
         var runtimeVersion = string.Empty;
         var runtimeNameAndVersionRegex = new Regex("([a-zA-Z.]*),Version=v([0-9.]*)");
@@ -89,44 +90,69 @@ internal class DatadogTestResultSerializer : ITestResultSerializer
 
                         if (result.TestCase.ExecutorUri.Host.IndexOf("xunit", StringComparison.OrdinalIgnoreCase) != -1)
                         {
+                            output.AppendLine("xUnit framework detected.");
                             testFramework = "xUnit";
                             var xunitCoreFile = Path.Combine(folder!, "xunit.core.dll");
+                            output.AppendLine("Looking for: " + xunitCoreFile);
                             if (File.Exists(xunitCoreFile))
                             {
+                                output.AppendLine("File exist, opening name.");
                                 var xunitCoreAssemblyName = AssemblyName.GetAssemblyName(xunitCoreFile);
                                 if (xunitCoreAssemblyName?.Version is { } version)
                                 {
+                                    output.AppendLine("Version extracted: " + version);
                                     testFrameworkVersion = version.ToString();
                                 }
+                            }
+                            else
+                            {
+                                output.AppendLine("File doesn't exist.");
                             }
                         }
                         else if (result.TestCase.ExecutorUri.Host.IndexOf("nunit", StringComparison.OrdinalIgnoreCase) != -1)
                         {
+                            output.AppendLine("NUnit framework detected.");
                             testFramework = "NUnit";
                             var nUnitFramework = Path.Combine(folder!, "nunit.framework.dll");
+                            output.AppendLine("Looking for: " + nUnitFramework);
                             if (File.Exists(nUnitFramework))
                             {
+                                output.AppendLine("File exist, opening name.");
                                 var nUnitFrameworkAssemblyName = AssemblyName.GetAssemblyName(nUnitFramework);
                                 if (nUnitFrameworkAssemblyName?.Version is { } version)
                                 {
+                                    output.AppendLine("Version extracted: " + version);
                                     testFrameworkVersion = version.ToString();
                                 }
+                            }
+                            else
+                            {
+                                output.AppendLine("File doesn't exist.");
                             }
                         }
                         else if (result.TestCase.ExecutorUri.Host.IndexOf("mstest", StringComparison.OrdinalIgnoreCase) != -1)
                         {
+                            output.AppendLine("MSTest framework detected.");
                             testFramework = "MSTestV2";
                             var msTestFramework = Path.Combine(folder!, "Microsoft.VisualStudio.TestPlatform.TestFramework.dll");
+                            output.AppendLine("Looking for: " + msTestFramework);
                             if (File.Exists(msTestFramework))
                             {
+                                output.AppendLine("File exist, opening name.");
                                 var msTestFrameworkAssemblyName = AssemblyName.GetAssemblyName(msTestFramework);
                                 if (msTestFrameworkAssemblyName?.Version is { } version)
                                 {
+                                    output.AppendLine("Version extracted: " + version);
                                     testFrameworkVersion = version.ToString();
                                 }
                             }
+                            else
+                            {
+                                output.AppendLine("File doesn't exist.");
+                            }
                         }
 
+                        output.AppendLine("Creating test module: " + testModuleName);
                         moduleStartTime = result.StartTime;
                         module = TestModule.Create(testModuleName, testFramework, testFrameworkVersion, moduleStartTime);
                         module.SetTag("runtime.name", runtimeName);
@@ -135,33 +161,39 @@ internal class DatadogTestResultSerializer : ITestResultSerializer
 
                     if (suite is null)
                     {
+                        output.AppendLine("  Creating test suite: " + testSuite);
                         suite = module.GetOrCreateSuite(testSuite, result.StartTime);
                     }
 
                     var testName = result.Method;
+                    output.AppendLine("    Creating test: " + testName);
                     var test = suite.CreateTest(testName, result.StartTime);
 
                     // Set test method
                     var methodName = testName;
                     if (methodName.IndexOf("(") != -1)
                     {
+                        output.AppendLine("      Test name has been modified (parameters).");
                         methodName = methodName.Substring(0, methodName.IndexOf("("));
                     }
 
                     var methodInfo = testModule.GetType(testSuite)?.GetMethod(methodName);
                     if (methodInfo is not null)
                     {
+                        output.AppendLine("      Setting Test Method Info.");
                         test.SetTestMethodInfo(methodInfo);
                     }
                     
                     // Traits
                     if (result.Traits.Any())
                     {
+                        output.AppendLine("      Setting test traits.");
                         var traits = result.Traits.GroupBy(k => k.Name).ToDictionary(k => k.Key, v => v.Select(i => i.Value).ToList());
                         test.SetTraits(traits);
                     }
 
                     // Set status
+                    output.AppendLine("    Closing test: " + testName);
                     var endTime = result.StartTime.Add(result.Duration);
                     if (endTime > suiteEndTime)
                     {
@@ -192,6 +224,7 @@ internal class DatadogTestResultSerializer : ITestResultSerializer
                     }
                 }
 
+                output.AppendLine("  Closing suite: " + testSuite);
                 suite?.Close(suiteEndTime.Subtract(suite.StartTime.DateTime));
                 if (moduleTime < suiteEndTime)
                 {
@@ -199,10 +232,11 @@ internal class DatadogTestResultSerializer : ITestResultSerializer
                 }
             }
 
+            output.AppendLine("Closing module: " + testModuleName);
             module?.Close(moduleTime.Subtract(moduleStartTime));
         }
 
-        return response;
+        return output.ToString();
     }
 
     private string? GetLoggerApiKey()
