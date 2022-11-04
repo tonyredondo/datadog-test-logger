229654aad5710811be2994231582dbc1c24eb140
Tony Redondo <tonyredondo@gmail.com>
2022-11-04 19:51:05 +0100

    Add test messages to the test instance. (#6)

--- src/Samples/XunitSample/UnitTest1.cs
+++ src/Samples/XunitSample/UnitTest1.cs
@@ -1,30 +1,44 @@
+using Xunit.Abstractions;
+
 namespace XunitSample;
 
 public class UnitTest1
 {
+    private ITestOutputHelper _output;
+
+    public UnitTest1(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     [Fact]
     public void Test1()
     {
+        _output.WriteLine("Test 1 message");
     }
 
     [Fact]
     public void Test2()
     {
+        _output.WriteLine("Test 2 message");
     }
 
     [Fact]
     public void Test3()
     {
+        _output.WriteLine("Test 3 message");
     }
 
     [Fact]
     public void Test4()
     {
+        _output.WriteLine("Test 4 message");
     }
 
     [Fact]
     public void Test5()
     {
+        _output.WriteLine("Test 5 message");
         var k = 0;
         var y = 18;
         var z = y / k;
