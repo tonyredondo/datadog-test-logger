876e9617c65afcb92d9276a02b7af587f91e7b78
Tony Redondo <tonyredondo@gmail.com>
2022-11-14 15:31:38 +0100

    Better exception type extractor. (#9)

--- build/Build.cs
+++ build/Build.cs
@@ -28,7 +28,7 @@ class Build : NukeBuild
     [Parameter("Where the NuGet package should be published")]
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
 
-    [Parameter] public string Version = "0.0.28"; 
+    [Parameter] public string Version = "0.0.29"; 
 
     Target Clean => _ => _
         .Executes(() =>
