d92b883e13e07b0d6a0910fc4a5e7a226e1b6fab
Tony Redondo <tonyredondo@gmail.com>
2022-11-25 12:15:55 +0100

    Include DateTime to the test logger log filename. (#13)

--- build/Build.cs
+++ build/Build.cs
@@ -28,7 +28,7 @@ class Build : NukeBuild
     [Parameter("Where the NuGet package should be published")]
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
 
-    [Parameter] public string Version = "0.0.31"; 
+    [Parameter] public string Version = "0.0.32"; 
 
     Target Clean => _ => _
         .Executes(() =>
